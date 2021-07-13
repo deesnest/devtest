4868d96d294f980c2bda5d9bf7a3519dffd84bee
Harshith <harshiththanush@gmail.com>
2021-07-13 05:24:25 +0000

    changes

--- rev.c
+++ rev.c
@@ -2,7 +2,7 @@
  int main()    
 {    
 int n, reverse=0, rem;    
-printf("Enter a number: ");    
+printf("\nEnter a number: ");    
   scanf("%d", &n);    
   while(n!=0)    
   {    
@@ -10,6 +10,6 @@ printf("Enter a number: ");
      reverse=reverse*10+rem;    
      n/=10;    
   }    
-  printf("Reversed Number: %d",reverse);    
+  printf("Reversed Number: %d"\n,reverse);    
 return 0;  
 }   
